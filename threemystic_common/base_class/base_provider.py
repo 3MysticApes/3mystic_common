de162392a10bf51474b7ae184b9b1e7ed2be15c6
Ron <tikicoder@gmail.com>
2023-07-08 21:36:13 -0700

    updates

--- threemystic_common/base_class/base_provider.py
+++ threemystic_common/base_class/base_provider.py
@@ -13,7 +13,7 @@ class base(main_base):
 
   @classmethod
   def get_nonprod_names(cls, *args, **kwargs):
-    return ["dev", "test", "stage", "nonprod", "non-prod"]
+    return ["dev", "test", "stage", "nonprod", "non-prod", "development"]
 
   @classmethod
   def get_supported_providers(cls, *args, **kwargs):
